ed7709ad241e7fde2af0a3f0bae43e9310264823
LLlKuIIeP <79032312801@yandex.ru>
2016-10-31 18:40:12 +0400

    Create solution.h

---
+++ Основы программирования для Linux/mod_1_step_2/solution.h
@@ -0,0 +1,10 @@
+#ifndef __SOLUTION__
+#define __SOLUTION__
+
+#ifdef __cplusplus
+	extern "C" int stringStat(const char *string, int multiplier, int *count);
+#else
+	int stringStat(const char *string, int multiplier, int *count);
+#endif
+
+#endif
