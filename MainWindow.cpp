5b0d913f1f4257b1044fee37c72d8f84b7689fa9
LLlKuIIeP <79032312801@yandex.ru>
2017-04-10 13:01:30 +0300

    Update MainWindow.cpp

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,10 +1,32 @@
-#include "MainWindow.h"
+MainWindow::MainWindow(QObject *parent) : QObject(parent), m_qwgt({}) {
+	m_qwgt.setWindowTitle("Hello world");
+	m_menu_bar = std::make_unique<QMenuBar>(&m_qwgt);
+	//m_menu_bar->addMenu("Меню");
 
-MainWindow::MainWindow(QObject *parent) : QObject(parent) {
-	qwgt.setWindowTitle("Hello world");
+//	std::unique_ptr<QMenu> menu = std::make_unique<QMenu>("menu");
+//	std::unique_ptr<QMenu> help = std::make_unique<QMenu>("help");
+
+//	m_top_menu.push_back(menu);
+//	m_top_menu.push_back(help);
+
+	m_top_menu.push_back(std::make_unique<QMenu>("menu"));
+	m_top_menu.push_back(std::make_unique<QMenu>("help"));
+
+	///m_menu_bar->addMenu(menu.get());
+	///std::unique_ptr<QMenu> help = std::make_unique<QMenu>("help");
+	///m_menu_bar->addMenu(help.get());
 
 }
 
 void MainWindow::show() {
-	qwgt.show();
+	for(auto &elem : m_top_menu) {
+		m_menu_bar->addMenu(elem.get());
+	}
+
+	m_qwgt.show();
+}
+
+void MainWindow::click_help()
+{
+	qDebug() << "\n\tClick \"help\"\n";
 }
