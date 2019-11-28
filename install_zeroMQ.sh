57eaff2c9933d9bfcdf2fbd0761dbcbcfc2bf2bb
LLlKuIIeP <9032312801@yandex.ru>
2019-11-28 23:44:57 +0300

    fix

--- install_zeroMQ.sh
+++ install_zeroMQ.sh
@@ -1,29 +1,4 @@
 #!/bin/bash
-sudo apt-get install \
-    g++ \
-    cmake \
-    libboost-all-dev \
-    libevent-dev \
-    libdouble-conversion-dev \
-    libgoogle-glog-dev \
-    libgflags-dev \
-    libiberty-dev \
-    liblz4-dev \
-    liblzma-dev \
-    libsnappy-dev \
-    make \
-    zlib1g-dev \
-    binutils-dev \
-    libjemalloc-dev \
-    libssl-dev \
-    pkg-config \
-    libsodium-dev -y
-
-sudo apt-get install \
-    libunwind8-dev \
-    libelf-dev \
-    libdwarf-dev -y
-  
 
 #!libzmq
 # or sudo apt-get install libczmq-dev -y
