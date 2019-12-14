ab395082c82c6e8aa22b7de6fa6fd00cc65ae6b7
LLlKuIIeP <9032312801@yandex.ru>
2019-12-15 00:55:29 +0300

    operator <<

---
+++ Шаблоны/Chapter_2/Stack/Stack.hpp
@@ -0,0 +1,74 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <cassert>
+
+
+// для объявления дружественной вне класса
+template<typename T>
+class Stack;
+template<typename T>
+std::ostream& operator<<(std::ostream& strm, Stack<T> const& s);
+// ----
+
+
+template<typename T>
+class Stack
+{
+private:
+    std::vector<T> elems;
+    static_assert(std::is_default_constructible_v<T>, "Класс Stack требует элементы, конструируемые по умолчпию");
+
+public:
+    // внутри можно без параметра шаблона
+    Stack() = default;
+    Stack(Stack const& lhs) : elems(lhs) {}
+    Stack& operator=(Stack const& lhs) { elems = lhs.elems; }
+
+    void push(T const& elem) { elems.push_back(elem); }
+    T pop();
+    T const& top() const;
+    bool empty() const { return elems.empty(); }
+
+    // operators
+    bool operator==(Stack const& lhs) { return elems == lhs.elems; }
+    // обычная, не шаблонная функция, инстанцируемая при необходимости
+    friend std::ostream& operator<<(std::ostream& strm, Stack<T> const& s) { s.printOn(strm); return strm; }
+    // объявления вне класса через отдельную шаблонную функцию, поэтому параметр шаблона отличный от Т
+    template<typename U>
+    friend std::ostream& operator<<(std::ostream& strm, Stack<U> const& s);
+    // параметр шаблона выступает как спецаялизация, но для этого нужно предварительное объявление
+    friend std::ostream& operator<< <T>(std::ostream& strm, Stack<T> const& s); // параметр Т после оператора означает специализацию шаблона
+    void printOn(std::ostream& strm) const noexcept;
+};
+
+template<typename T>
+std::ostream& operator<<(std::ostream& strm, Stack<T> const& s)
+{
+    s.printOn(strm);
+    return strm;
+}
+
+template<typename T>
+T Stack<T>::pop()
+{
+    assert(!elems.empty());
+    T elem = elems.back();
+    elems.pop_back();
+    return elem;
+}
+
+template<typename T>
+T const& Stack<T>::top() const
+{
+    assert(!elems.empty());
+    return elems.back();
+}
+
+template<typename T>
+void Stack<T>::printOn(std::ostream& strm) const noexcept
+{
+    for(T const& elem : elems) {
+        strm << elem << ' ';
+    }
+}
--- Шаблоны/Chapter_2/Stack/main.cpp
+++ Шаблоны/Chapter_2/Stack/main.cpp
@@ -1,52 +1,21 @@
 #include <iostream>
-#include <vector>
-#include <cassert>
-
-
-template<typename T>
-class Stack
-{
-private:
-    std::vector<T> elems;
-
-public:
-    // внутри можно без параметра шаблона
-    Stack(Stack const& lhs) : elems(lhs) {}
-    Stack& operator=(Stack const& lhs)
-    {
-        elems = lhs.elems;
-    }
-
-    void push(T const& elem);
-    void pop();
-    T const& top() const;
-    bool empty() const
-    {
-        return elems.emoty();
-    }
-};
-
-template<typename T>
-void Stack<T>::push(T const& elem)
-{
-    elems.push_back(elem);
-}
-template<typename T>
-void Stack<T>::pop()
-{
-    assert(!elems.empty());
-    elems.pop_back();
-}
-template<typename T>
-T const& Stack<T>::top() const
-{
-    assert(!elems.empty());
-    return elems.back();
-}
+#include <Stack.hpp>
 
 
 int main()
 {
     std::cout << "Hello World!" << std::endl;
+
+    Stack<double> s1;
+    Stack<double> s2;
+    void(s1 == s2);
+
+    s1.push(1.);
+    s1.printOn(std::cout);
+    std::cout << std::endl;
+
+    s2.push(2.);
+    std::cout << s2 << std::endl;
+
     return 0;
 }
