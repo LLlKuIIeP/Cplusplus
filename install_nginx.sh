b98f360b6e6debafc7c329980ddd95ad7f6d7ea8
LLlKuIIeP <9032312801@yandex.ru>
2020-02-24 18:25:08 +0300

    nginx

--- install_nginx.sh
+++ install_nginx.sh
@@ -13,21 +13,25 @@ sudo mkdir -p /var/www/doxygen
 sudo chown -R $USER:$USER /var/www/doxygen
 sudo chmod a+rwx -R /var/www/doxygen
 
+local_ip=ifconfig | grep -o "inet [0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" | grep -o "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" | head -n1
+
 sudo touch /etc/nginx/sites-available/doxygen
 sudo chmod a+rw /etc/nginx/sites-available/doxygen
+
+local_ip=`ifconfig | grep -o "inet [0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" | grep -o "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" | head -n1`
 sudo echo -e "#log_format simple '\$remote_addr \$request \$status';\n" > /etc/nginx/sites-available/doxygen
 sudo echo -e "server {\n" >> /etc/nginx/sites-available/doxygen
 sudo echo -e "\tlisten 80;\n" >> /etc/nginx/sites-available/doxygen
 sudo echo -e "\tlisten [::]:80;\n\n" >> /etc/nginx/sites-available/doxygen
 sudo echo -e "\tindex index.html index.htm index.nginx-debian.html;\n\n" >> /etc/nginx/sites-available/doxygen
-sudo echo -e "\tserver_name 192.168.0.10;\n\n" >> /etc/nginx/sites-available/doxygen
+sudo echo -e "\tserver_name $local_ip;\n\n" >> /etc/nginx/sites-available/doxygen
 sudo echo -e "\t#error_log /var/log/nginx/error.log info;\n" >> /etc/nginx/sites-available/doxygen
 sudo echo -e "\t#access_log /var/log/nginx/access.log simple;\n\n" >> /etc/nginx/sites-available/doxygen
 sudo echo -e "\tlocation /doxygen {\n" >> /etc/nginx/sites-available/doxygen
 sudo echo -e "\t\t#access_log /var/log/nginx/access.log simple;\n" >> /etc/nginx/sites-available/doxygen
 sudo echo -e "\t\troot /var/www;\n" >> /etc/nginx/sites-available/doxygen
 sudo echo -e "\t\tindex index.html index.htm;\n" >> /etc/nginx/sites-available/doxygen
-sudo echo -e "\t\ttry_files $uri $uri/ =404;\n" >> /etc/nginx/sites-available/doxygen
+sudo echo -e "\t\ttry_files \$uri \$uri/ =404;\n" >> /etc/nginx/sites-available/doxygen
 sudo echo -e "\t}\n" >> /etc/nginx/sites-available/doxygen
 sudo echo -e "}\n" >> /etc/nginx/sites-available/doxygen
 
